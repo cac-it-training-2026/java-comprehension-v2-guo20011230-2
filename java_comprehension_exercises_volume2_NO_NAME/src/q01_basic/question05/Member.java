caf05ea9c05eb34651e0f369675d5f52613b8b52
yupuguo <yupuguo@C010423081.zipangu.cac.co.jp>
2026-05-01 09:27:07 +0900

    question05～question06完了

---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/Member.java
@@ -0,0 +1,72 @@
+package q01_basic.question05;
+
+public class Member extends AbstMember {
+
+	// ① 字段
+	private int id;
+	private String password;
+	private int age;
+	private int rank;
+
+	// ② 构造器
+	public Member() {
+	}
+
+	public Member(int id, String password, String name, int age, int rank) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+	}
+
+	// ③ getter / setter
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	// ④ override
+	@Override
+	public void buyItem() {
+		System.out.println(name + " purchased the item at 50% off");
+	}
+
+	@Override
+	public void showMember() {
+		System.out.println("***MEMBER DATA***");
+		System.out.println("id:" + id);
+		System.out.println("password:" + password);
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		System.out.println("rank:" + rank);
+		System.out.println("*****************");
+	}
+}
